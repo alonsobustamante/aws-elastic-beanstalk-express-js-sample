b8e9043d6eff080bcb6ff7c1997cca80ec7cd343
Alonso Bustamante <carlosaj.bustamante@gmail.com>
2024-04-06 23:14:45 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Alons, este es tu primer pipeline CI/CD'));
+app.get('/', (req, res) => res.send('Hola Alonso, se agrego una revision Manual'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
